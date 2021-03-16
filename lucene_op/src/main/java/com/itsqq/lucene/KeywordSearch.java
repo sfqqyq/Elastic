cac9c6bb50a96f1d8f7ff956dc4da254aacd0ff3
sfqqyq <sfqqyq@163.com>
2021-03-16 23:37:19 +0800

    使用lucene通过索引查询文档

---
+++ lucene_op/src/main/java/com/itsqq/lucene/KeywordSearch.java
@@ -0,0 +1,53 @@
+package com.itsqq.lucene;
+
+import jdk.nashorn.internal.ir.CallNode;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.FSDirectory;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+/**
+ * 基于前面Lcuene建立的索引库来进行全文检索
+ * 根据关键字进行检索
+ */
+public class KeywordSearch {
+    public static void main(String[] args) throws IOException {
+        // 1、使用DirectoryReader.open 构建索引读取器
+        DirectoryReader reader = DirectoryReader.open(FSDirectory.open(Paths.get("/Users/mac/IdeaProjects/Elastic/lucene_op/index")));
+
+        // 2、构建索引查询器（IndexSearcher）
+        // 用来搜索关键字的
+        IndexSearcher indexSearcher = new IndexSearcher(reader);
+
+        // 3、构建词条（Term）和词条查询（TermQuery）
+        TermQuery termQuery = new TermQuery(new Term("content", "跳"));
+
+        // 4、执行查询，获取文档
+        TopDocs topDocs = indexSearcher.search(termQuery, 50);
+
+        // 5、遍历打印文档（可以使用IndexSearch.doc根据文档id获取文档）
+        ScoreDoc[] scoreDocs = topDocs.scoreDocs;
+        for (ScoreDoc scoreDoc : scoreDocs) {
+            // Lucene中，每一个文档都有一个唯一ID
+            // 根据唯一ID就可以获取到文档
+            Document doc = indexSearcher.doc(scoreDoc.doc);
+            // 获取文档中的字段
+            System.out.println("-------------");
+            System.out.println("文件名："+doc.get("file_name"));
+            System.out.println("文件路径："+doc.get("path"));
+            System.out.println("文件内容："+doc.get("content"));
+        }
+
+        // 6、关闭索引读取器
+        reader.close();
+
+    }
+
+}
